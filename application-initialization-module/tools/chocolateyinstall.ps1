411324128582bc7473a5f03e3c173976f91456f1
neutmute <neutmute@users.noreply.github.com>
2016-03-24 15:35:46 +1100

    app init module

---
+++ application-initialization-module/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'application-initialization-module' 
+$registryUninstallerKeyName = 'application-initialization-module' 
+$installerType = 'msi' 
+$url = 'http://go.microsoft.com/fwlink/?LinkID=247816' 
+$url64 = 'http://go.microsoft.com/fwlink/?LinkID=247817'
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
---
+++ application-initialization-module/tools/chocolateyuninstall.ps1
@@ -0,0 +1,20 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+# Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+
+$packageName = 'application-initialization-module'
+$registryUninstallerKeyName = 'application-initialization-module' #ensure this is the value in the registry
+$installerType = 'EXE'
+$silentArgs = '/S'
+$validExitCodes = @(0)
+
+$osBitness = Get-ProcessorBits
+
+if ($osBitness -eq 64) {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} else {
+  $file = (Get-ItemProperty -Path "hklm:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$registryUninstallerKeyName").UninstallString
+} 
+	
+Uninstall-ChocolateyPackage -PackageName $packageName -FileType $installerType -SilentArgs $silentArgs -validExitCodes $validExitCodes -File $file
