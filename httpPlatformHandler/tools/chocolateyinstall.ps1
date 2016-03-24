15e71b7877cdc201c43b0139b268c825635338a4
neutmute <neutmute@users.noreply.github.com>
2016-03-24 15:15:17 +1100

    platform handler

---
+++ httpPlatformHandler/tools/chocolateyinstall.ps1
@@ -0,0 +1,13 @@
+﻿# stop on all errors
+$ErrorActionPreference = 'Stop';
+
+$packageName = 'HttpPlatformHandler' 
+$registryUninstallerKeyName = 'HttpPlatformHandler' 
+$installerType = 'msi' 
+$url = 'http://go.microsoft.com/fwlink/?LinkId=690722' 
+$url64 = 'http://go.microsoft.com/fwlink/?LinkId=690721'
+$silentArgs = '/quiet' 
+$validExitCodes = @(0) 
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64"  -validExitCodes $validExitCodes
