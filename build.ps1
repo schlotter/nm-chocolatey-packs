a6e4adda381298f57cf14642bef611130440f9f0
neutmute <neutmute@users.noreply.github.com>
2016-03-31 22:42:13 +1100

    wintail package

--- build.ps1
+++ build.ps1
@@ -1,6 +1,11 @@
 Write-Host "-=Build all chocolatey packs=-"
 
 $nuspecs = Get-ChildItem -Path $PSScriptRoot -Filter *.nuspec -Recurse
+
 Foreach($nuspec in $nuspecs){
     choco pack $nuspec.FullName
 }
+
+$artifactsFolder = "./.artifacts"
+New-Item $artifactsFolder -Force -Type Directory | Out-Null
+Move-Item *.nupkg $artifactsFolder
---
+++ wintail/tools/chocolateyinstall.ps1
@@ -0,0 +1,7 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$url = 'http://www.baremetalsoft.com/wintail/download.php?p=p'  
+$destination = "$env:ChocolateyInstall\lib\wintail\wintail.exe"
+
+Get-WebFile -url $url -filename $destination
+Install-ChocolateyShortcut -TargetPath $destination -ShortcutFilePath "$env:appdata\Microsoft\Windows\SendTo\WinTail.lnk"
