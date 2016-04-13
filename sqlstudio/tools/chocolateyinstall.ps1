34d26c2badaebf03a570f7b0b323954719dec8cc
neutmute <neutmute@users.noreply.github.com>
2016-04-13 22:49:42 +1000

    fix log path

--- sqlstudio/tools/chocolateyinstall.ps1
+++ sqlstudio/tools/chocolateyinstall.ps1
@@ -3,9 +3,9 @@
 $packageName = 'sqlstudio' 
 $registryUninstallerKeyName = 'sqlstudio' 
 $installerType = 'exe' 
-$silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 $validExitCodes = @(0) 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 
 if (!(Test-Path env:\chocolatey:sqlstudio:url)){$env:chocolatey:sqlstudio:url="http://go.microsoft.com/fwlink/?LinkId=690722"}
 
