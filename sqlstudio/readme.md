a2e8cad665c859819b6436d720aea86de3c9b2e9
neutmute <neutmute@users.noreply.github.com>
2016-04-20 20:12:02 +1000

    no env vars

--- sqlstudio/readme.md
+++ sqlstudio/readme.md
@@ -1,14 +1,5 @@
 # SQL Server Management Studio (SSMS) Package
 SQL Server Management Studio (SSMS) is an integrated environment for accessing, configuring, managing, administering, and developing all components of SQL Server. 
 
-##Example install
-From elevated PowerShell:
-
-	# optional if you have the 800MB download cached locally
-	$env:chocolatey:sqlstudio:url="D:\Downloads\SSMS-Setup-ENU.exe" 
-
-	# required
-	choco install sqlstudio
-
 ##Prerequisites
 [KB2919355](https://support.microsoft.com/en-au/kb/2919355) for applicable operating systems
--- sqlstudio/tools/chocolateyinstall.ps1
+++ sqlstudio/tools/chocolateyinstall.ps1
@@ -1,5 +1,4 @@
-﻿# stop on all errors
-
+﻿
 $packageName = 'sqlstudio' 
 $registryUninstallerKeyName = 'sqlstudio' 
 $installerType = 'exe' 
@@ -7,6 +6,6 @@ $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 
-if (!(Test-Path env:\chocolatey:sqlstudio:url)){$env:chocolatey:sqlstudio:url="http://go.microsoft.com/fwlink/?LinkID=785065"}
+$url="http://go.microsoft.com/fwlink/?LinkID=786460"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$env:chocolatey:sqlstudio:url" -validExitCodes $validExitCodes
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
