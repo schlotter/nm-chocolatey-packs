0b24d1e4136a4326a7187ff0d57e85bb929d549f
neutmute <neutmute@users.noreply.github.com>
2016-06-02 06:46:20 +1000

    sql management studio RTM

--- sqlstudio/tools/chocolateyinstall.ps1
+++ sqlstudio/tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 
-$url="http://go.microsoft.com/fwlink/?LinkID=786460"
+$url="http://download.microsoft.com/download/E/D/3/ED3B06EC-E4B5-40B3-B861-996B710A540C/SSMS-Setup-ENU.exe"
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
