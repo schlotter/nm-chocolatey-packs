048795123e44cd4e9db07b89b92e99ec5e80b4c3
neutmute <neutmute@users.noreply.github.com>
2016-07-06 16:14:42 +1000

    july build

--- sqlstudio/tools/chocolateyinstall.ps1
+++ sqlstudio/tools/chocolateyinstall.ps1
@@ -6,6 +6,6 @@ $validExitCodes = @(0)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 
-$url="http://download.microsoft.com/download/E/D/3/ED3B06EC-E4B5-40B3-B861-996B710A540C/SSMS-Setup-ENU.exe"
+$url="http://download.microsoft.com/download/6/F/C/6FCFDC7F-772F-4FEF-BD48-D75C9A3CFB54/SSMS-Setup-ENU.exe"
 
 Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
