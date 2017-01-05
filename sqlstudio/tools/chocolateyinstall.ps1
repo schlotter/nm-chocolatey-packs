8ad39563314845e5aa5496c479815a924d92fe76
neutmute <neutmute@users.noreply.github.com>
2017-01-06 09:54:04 +1100

    update with hashes

--- sqlstudio/tools/chocolateyinstall.ps1
+++ sqlstudio/tools/chocolateyinstall.ps1
@@ -1,11 +1,21 @@
-﻿
-$packageName = 'sqlstudio' 
+﻿$packageName = 'sqlstudio' 
 $registryUninstallerKeyName = 'sqlstudio' 
 $installerType = 'exe' 
 $validExitCodes = @(0) 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $silentArgs = "/install /quiet /norestart /log $toolsDir\..\log.txt"
 
-$url="http://download.microsoft.com/download/6/F/C/6FCFDC7F-772F-4FEF-BD48-D75C9A3CFB54/SSMS-Setup-ENU.exe"
+$url="https://download.microsoft.com/download/3/1/D/31D734E0-BFE8-4C33-A9DE-2392808ADEE6/SSMS-Setup-ENU.exe"
 
-Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = $installerType
+  url           = $url
+  silentArgs    = $silentArgs
+  validExitCodes= $validExitCodes
+  checksum      = '243E996D4015733922AFC0D14C81A517BA1A23552BA0F401624EFC3B7C367366'
+  checksumType  = 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
+    
