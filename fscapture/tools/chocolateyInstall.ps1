b65490b3e98ffb7c5b267a7e48bc26ed5a586e38
neutmute <neutmute@users.noreply.github.com>
2017-05-21 07:35:49 +1000

    fscapture

---
+++ fscapture/tools/chocolateyInstall.ps1
@@ -0,0 +1,25 @@
+﻿$ErrorActionPreference = 'Stop';
+
+#cinst checksum
+#checksum -t=sha1 FSCaptureSetup85.exe
+
+#test with:
+#cinst fscapture.8.5.nupkg --force
+
+$packageName= 'fscapture'
+$toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$url        = 'http://www.faststonesoft.net/DN/FSCaptureSetup85.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'EXE'
+  url           = $url
+
+  silentArgs 	= "/S"
+
+  softwareName  = 'FastStone Image Capture'
+  checksum      = 'B30A1D9C2DED7F914CEC4C65A0507FC294F193F0'
+  checksumType  = 'sha1'
+}
+
+Install-ChocolateyPackage @packageArgs
