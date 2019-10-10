0ba5f9790faf42e2579d330f084f9fd4e87d93ba
Jérémie Bertrand <jeremie.bertrand@gmail.com>
2019-10-10 16:11:44 +0200

    Update Agent Ransack to latest version
    And use the zip/msi installers.

--- agentransack/tools/chocolateyInstall.ps1
+++ agentransack/tools/chocolateyInstall.ps1
@@ -1,36 +1,37 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 #cinst checksum
-#checksum -t=sha1 AgentRansack_nnn.exe
+#checksum -t=sha1 AgentRansack_x86_nnnn.zip
+#checksum -t=sha1 AgentRansack_x64_nnnn.zip
 
 #test with:
-#cinst agentransack.8.0.867.0.nupkg --force
+#cinst agentransack.2019.2929.nupkg --force
 
 $packageName= 'AgentRansack'
 $toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url        = 'https://download.mythicsoft.com/ar/867/AgentRansack_867.exe'
+$url        = 'https://download.mythicsoft.com/flp/2929/agentransack_x86_msi_2929.zip'
+$url64      = 'https://download.mythicsoft.com/flp/2929/agentransack_x64_msi_2929.zip'
+$fileLocation = Join-Path $toolsDir 'agentransack_x86_2929.msi'
+$fileLocation64 = Join-Path $toolsDir 'agentransack_x64_2929.msi'
 
 $packageArgs = @{
   packageName   = $packageName
-  fileType      = 'EXE'
+  fileType      = 'MSI'
   url           = $url
+  url64         = $url64
+  unzipLocation = $toolsDir
+  file          = $fileLocation
+  file64        = $fileLocation64
 
-  silentArgs 	= ""
-
+  silentArgs    = "/quiet"
+  validExitCodes= @(0)
+  
   softwareName  = 'AgentRansack'
-  checksum      = '6AF2115B473FDE77A9DC958DE4FF730EB25810EA'
+  checksum      = 'CF9DC1E831DA98AB04B802EF8C4D486F08E4F2B4'
   checksumType  = 'sha1'
+  checksum64    = '20265BD24EA276D60A18B001AD1D96D792437A9A'
+  checksumType64= 'sha1'
 }
 
-
-$ahkExe = 'AutoHotKey'
-$ahkFile = Join-Path $toolsDir "agent-ransack-install.ahk"
-$ahkProc = Start-Process -FilePath $ahkExe `
-                         -ArgumentList $ahkFile `
-                         -PassThru
-
-$ahkId = $ahkProc.Id
-Write-Debug "$ahkExe start time:`t$($ahkProc.StartTime.ToShortTimeString())"
-Write-Debug "Process ID:`t$ahkId"
-
-Install-ChocolateyPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
+Install-ChocolateyInstallPackage @packageArgs
