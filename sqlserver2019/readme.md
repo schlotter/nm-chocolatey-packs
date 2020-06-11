1ced0169db2bc587fda253ae66807fcd5cfa1a37
neutmute <neutmute@users.noreply.github.com>
2020-06-11 12:46:22 +1000

    2019

--- sqlserver2019/readme.md
+++ sqlserver2019/readme.md
@@ -1,34 +1,26 @@
-# Adding a new SQL Server Version
+# SQL Server 2019 Chocolatey Package
+* Uses environment variables to configure.
+* Minimum requirement is a local path to the ISO image (not publicly available at time of writing).
+* Path to the ISO is supplied to the package via the `choco:sqlserver2019:isoImage` environment variable.
 
-* Everything is orchestrated from the `tools\chocolateyinstall.ps1` powershell.
-* A helper xlsx is used to generate the code that looks for env vars and to set the params
+See `chocolateyinstall.ps1` for all environment vars which mirror those in the [documentation](https://msdn.microsoft.com/en-us/library/ms144259.aspx)
 
-## Steps to create
+##Example install
+From elevated PowerShell:
 
-1. Clone structure of `sqlNext` folder and rename to `sql`
-1. Rename `sqlserverNext.nuspec` to `sqlserver{new_version}.nuspec`
-1. Edit `*.nuspec` as required
-    * Search and replace for `{Next}` with `{new_version}`
-    * Add url to product page as `projectUrl`
-    * Update `description`
-1. Rename `docs\sqlNext_ParamGenerator.xlsx` to `docs\sql{new_version}_ParamGenerator.xlsx`
-1. Update version in D2
-1. Find the new version in the `Other Versions` dropdown on [MSDN](https://msdn.microsoft.com/en-us/library/ms144259(v=sql.130).aspx) and copy/paste to populate `docs\sql{new_version}_ParamGenerator.xlsx`
-    * Copy/paste first 2 column of table in `Install Parameters` into columns A & B
-    * Copy/paste description column into Column F.
+	$env:choco:sqlserver2019:isoImage="D:\Downloads\SQLServer2019-x64-ENU-Dev.iso"
 
-> Hint: Uploading to Google Sheets and copy/pasting tables is less tedious
+	$env:choco:sqlserver2019:PID="22222-00000-00000-00000-00000" # Developer edition
+	$env:choco:sqlserver2019:INSTANCEID="sql2019"
+	$env:choco:sqlserver2019:INSTANCENAME="sql2019"
+	$env:choco:sqlserver2019:AGTSVCACCOUNT="NT Service\SQLAgent`$SQL2019"
+	$env:choco:sqlserver2019:SQLSVCACCOUNT="NT Service\MSSQL`$SQL2019"
+	$env:choco:sqlserver2019:INSTALLSQLDATADIR="C:\Data\Sql"
 
-> Hint 2: Column B `Parameter` should only have one line break in it, if you notice it being split over more than Columns C & D, then you will need to clean it up.
+	# If you want mixed mode auth, you must supply a password for sa, otherwise skip these two configs
+	$env:choco:sqlserver2019:SECURITYMODE="SQL"
+	$env:choco:sqlserver2019:SAPWD="0wn3d"
 
-6. Enter appropriate values for Columns E, G & K
-1. Columns L, M generate Powershell for pasting into `tools\chocolateyinstall.ps1`
-    * Copy/paste Column L under the comment `# Copy/paste 'Set Default' column under here`
-    * Copy/paste Column M under the comment `# Copy/paste 'Write Env Var to config' column under here`
-1. Search and replace `tools\chocolateyinstall.ps1` for `{Next}` with `{new_version}`
-1. Delete/rename `readme.md` and rename `readme.md.final` to `readme.md`
-1. Update `readme.md` as required
-    * Search and replace for `{Next}` with `{new_version}`
-    * Add any prerequistes
+	choco install sqlserver2019 -Source "https://www.myget.org/F/nm-chocolatey-packs/api/v2"
 
->You will need to find out how to install developer edition from CLI. This has changed through SQL editions. SQL2014 may like be SQL2016 (Special serial) or maybe SQL2012 (product edition).
+##Prerequisites
--- sqlserver2019/tools/chocolateyinstall.ps1
+++ sqlserver2019/tools/chocolateyinstall.ps1
@@ -17,11 +17,109 @@ function BuildConfigurationFile()
     ## Below generated from https://github.com/neutmute/nm-chocolatey-packs/raw/master/sqlserver2019/docs/sql2019_ParamGenerator.xlsx
 	##
 
-    # If user didn't supply some basic requirements, set them
-    # Copy/paste 'Set Default' column under here
+    if (!(Test-Path env:\choco:sqlserver2019:ACTION)){$env:choco:sqlserver2019:ACTION="Install"}
+    if (!(Test-Path env:\choco:sqlserver2019:SUPPRESSPRIVACYSTATEMENTNOTICE)){$env:choco:sqlserver2019:SUPPRESSPRIVACYSTATEMENTNOTICE="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:IACCEPTSQLSERVERLICENSETERMS)){$env:choco:sqlserver2019:IACCEPTSQLSERVERLICENSETERMS="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:IACCEPTPYTHONLICENSETERMS)){$env:choco:sqlserver2019:IACCEPTPYTHONLICENSETERMS="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:IACCEPTROPENLICENSETERMS)){$env:choco:sqlserver2019:IACCEPTROPENLICENSETERMS="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:ENU)){$env:choco:sqlserver2019:ENU="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:FEATURES)){$env:choco:sqlserver2019:FEATURES="SQLEngine"}
+    if (!(Test-Path env:\choco:sqlserver2019:PID)){$env:choco:sqlserver2019:PID="22222-00000-00000-00000-00000"}
+    if (!(Test-Path env:\choco:sqlserver2019:QUIET)){$env:choco:sqlserver2019:QUIET="TRUE"}
+    if (!(Test-Path env:\choco:sqlserver2019:INSTANCENAME)){$env:choco:sqlserver2019:INSTANCENAME="MSSQLSERVER"}
+    
 
-    # Write config to file
-    # Copy/paste 'Write Env Var to config' column under here
+    if (Test-Path env:\choco:sqlserver2019:ACTION){Add-Content $configFile "ACTION=`"$env:choco:sqlserver2019:ACTION`""}
+    if (Test-Path env:\choco:sqlserver2019:SUPPRESSPRIVACYSTATEMENTNOTICE){Add-Content $configFile "SUPPRESSPRIVACYSTATEMENTNOTICE=`"$env:choco:sqlserver2019:SUPPRESSPRIVACYSTATEMENTNOTICE`""}
+    if (Test-Path env:\choco:sqlserver2019:IACCEPTSQLSERVERLICENSETERMS){Add-Content $configFile "IACCEPTSQLSERVERLICENSETERMS=`"$env:choco:sqlserver2019:IACCEPTSQLSERVERLICENSETERMS`""}
+    if (Test-Path env:\choco:sqlserver2019:IACCEPTPYTHONLICENSETERMS){Add-Content $configFile "IACCEPTPYTHONLICENSETERMS=`"$env:choco:sqlserver2019:IACCEPTPYTHONLICENSETERMS`""}
+    if (Test-Path env:\choco:sqlserver2019:IACCEPTROPENLICENSETERMS){Add-Content $configFile "IACCEPTROPENLICENSETERMS=`"$env:choco:sqlserver2019:IACCEPTROPENLICENSETERMS`""}
+    if (Test-Path env:\choco:sqlserver2019:ENU){Add-Content $configFile "ENU=`"$env:choco:sqlserver2019:ENU`""}
+    if (Test-Path env:\choco:sqlserver2019:UpdateEnabled){Add-Content $configFile "UpdateEnabled=`"$env:choco:sqlserver2019:UpdateEnabled`""}
+    if (Test-Path env:\choco:sqlserver2019:UpdateSource){Add-Content $configFile "UpdateSource=`"$env:choco:sqlserver2019:UpdateSource`""}
+    if (Test-Path env:\choco:sqlserver2019:CONFIGURATIONFILE){Add-Content $configFile "CONFIGURATIONFILE=`"$env:choco:sqlserver2019:CONFIGURATIONFILE`""}
+    if (Test-Path env:\choco:sqlserver2019:ERRORREPORTING){Add-Content $configFile "ERRORREPORTING=`"$env:choco:sqlserver2019:ERRORREPORTING`""}
+    if (Test-Path env:\choco:sqlserver2019:FEATURES){Add-Content $configFile "FEATURES=`"$env:choco:sqlserver2019:FEATURES`""}
+    if (Test-Path env:\choco:sqlserver2019:HELP){Add-Content $configFile "HELP=`"$env:choco:sqlserver2019:HELP`""}
+    if (Test-Path env:\choco:sqlserver2019:INDICATEPROGRESS){Add-Content $configFile "INDICATEPROGRESS=`"$env:choco:sqlserver2019:INDICATEPROGRESS`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTALLSHAREDDIR){Add-Content $configFile "INSTALLSHAREDDIR=`"$env:choco:sqlserver2019:INSTALLSHAREDDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTALLSHAREDWOWDIR){Add-Content $configFile "INSTALLSHAREDWOWDIR=`"$env:choco:sqlserver2019:INSTALLSHAREDWOWDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTANCEDIR){Add-Content $configFile "INSTANCEDIR=`"$env:choco:sqlserver2019:INSTANCEDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTANCEID){Add-Content $configFile "INSTANCEID=`"$env:choco:sqlserver2019:INSTANCEID`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTANCENAME){Add-Content $configFile "INSTANCENAME=`"$env:choco:sqlserver2019:INSTANCENAME`""}
+    if (Test-Path env:\choco:sqlserver2019:PBENGSVCACCOUNT){Add-Content $configFile "PBENGSVCACCOUNT=`"$env:choco:sqlserver2019:PBENGSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:PBDMSSVCPASSWORD){Add-Content $configFile "PBDMSSVCPASSWORD=`"$env:choco:sqlserver2019:PBDMSSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:PBENGSVCSTARTUPTYPE){Add-Content $configFile "PBENGSVCSTARTUPTYPE=`"$env:choco:sqlserver2019:PBENGSVCSTARTUPTYPE`""}
+    if (Test-Path env:\choco:sqlserver2019:PBPORTRANGE){Add-Content $configFile "PBPORTRANGE=`"$env:choco:sqlserver2019:PBPORTRANGE`""}
+    if (Test-Path env:\choco:sqlserver2019:PBSCALEOUT){Add-Content $configFile "PBSCALEOUT=`"$env:choco:sqlserver2019:PBSCALEOUT`""}
+    if (Test-Path env:\choco:sqlserver2019:PID){Add-Content $configFile "PID=`"$env:choco:sqlserver2019:PID`""}
+    if (Test-Path env:\choco:sqlserver2019:Q or QUIET){Add-Content $configFile "Q or QUIET=`"$env:choco:sqlserver2019:Q or QUIET`""}
+    if (Test-Path env:\choco:sqlserver2019:QS or QUIETSIMPLE){Add-Content $configFile "QS or QUIETSIMPLE=`"$env:choco:sqlserver2019:QS or QUIETSIMPLE`""}
+    if (Test-Path env:\choco:sqlserver2019:UIMODE){Add-Content $configFile "UIMODE=`"$env:choco:sqlserver2019:UIMODE`""}
+    if (Test-Path env:\choco:sqlserver2019:SQMREPORTING){Add-Content $configFile "SQMREPORTING=`"$env:choco:sqlserver2019:SQMREPORTING`""}
+    if (Test-Path env:\choco:sqlserver2019:HIDECONSOLE){Add-Content $configFile "HIDECONSOLE=`"$env:choco:sqlserver2019:HIDECONSOLE`""}
+    if (Test-Path env:\choco:sqlserver2019:AGTSVCACCOUNT){Add-Content $configFile "AGTSVCACCOUNT=`"$env:choco:sqlserver2019:AGTSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:AGTSVCPASSWORD){Add-Content $configFile "AGTSVCPASSWORD=`"$env:choco:sqlserver2019:AGTSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:AGTSVCSTARTUPTYPE){Add-Content $configFile "AGTSVCSTARTUPTYPE=`"$env:choco:sqlserver2019:AGTSVCSTARTUPTYPE`""}
+    if (Test-Path env:\choco:sqlserver2019:ASBACKUPDIR){Add-Content $configFile "ASBACKUPDIR=`"$env:choco:sqlserver2019:ASBACKUPDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:ASCOLLATION){Add-Content $configFile "ASCOLLATION=`"$env:choco:sqlserver2019:ASCOLLATION`""}
+    if (Test-Path env:\choco:sqlserver2019:ASCONFIGDIR){Add-Content $configFile "ASCONFIGDIR=`"$env:choco:sqlserver2019:ASCONFIGDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:ASDATADIR){Add-Content $configFile "ASDATADIR=`"$env:choco:sqlserver2019:ASDATADIR`""}
+    if (Test-Path env:\choco:sqlserver2019:ASLOGDIR){Add-Content $configFile "ASLOGDIR=`"$env:choco:sqlserver2019:ASLOGDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:ASSERVERMODE){Add-Content $configFile "ASSERVERMODE=`"$env:choco:sqlserver2019:ASSERVERMODE`""}
+    if (Test-Path env:\choco:sqlserver2019:ASSVCACCOUNT){Add-Content $configFile "ASSVCACCOUNT=`"$env:choco:sqlserver2019:ASSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:ASSVCPASSWORD){Add-Content $configFile "ASSVCPASSWORD=`"$env:choco:sqlserver2019:ASSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:ASSVCSTARTUPTYPE){Add-Content $configFile "ASSVCSTARTUPTYPE=`"$env:choco:sqlserver2019:ASSVCSTARTUPTYPE`""}
+    if (Test-Path env:\choco:sqlserver2019:ASSYSADMINACCOUNTS){Add-Content $configFile "ASSYSADMINACCOUNTS=`"$env:choco:sqlserver2019:ASSYSADMINACCOUNTS`""}
+    if (Test-Path env:\choco:sqlserver2019:ASTEMPDIR){Add-Content $configFile "ASTEMPDIR=`"$env:choco:sqlserver2019:ASTEMPDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:ASPROVIDERMSOLAP){Add-Content $configFile "ASPROVIDERMSOLAP=`"$env:choco:sqlserver2019:ASPROVIDERMSOLAP`""}
+    if (Test-Path env:\choco:sqlserver2019:FARMACCOUNT){Add-Content $configFile "FARMACCOUNT=`"$env:choco:sqlserver2019:FARMACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:FARMPASSWORD){Add-Content $configFile "FARMPASSWORD=`"$env:choco:sqlserver2019:FARMPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:PASSPHRASE){Add-Content $configFile "PASSPHRASE=`"$env:choco:sqlserver2019:PASSPHRASE`""}
+    if (Test-Path env:\choco:sqlserver2019:FARMADMINIPORT){Add-Content $configFile "FARMADMINIPORT=`"$env:choco:sqlserver2019:FARMADMINIPORT`""}
+    if (Test-Path env:\choco:sqlserver2019:BROWSERSVCSTARTUPTYPE){Add-Content $configFile "BROWSERSVCSTARTUPTYPE=`"$env:choco:sqlserver2019:BROWSERSVCSTARTUPTYPE`""}
+    if (Test-Path env:\choco:sqlserver2019:ENABLERANU){Add-Content $configFile "ENABLERANU=`"$env:choco:sqlserver2019:ENABLERANU`""}
+    if (Test-Path env:\choco:sqlserver2019:INSTALLSQLDATADIR){Add-Content $configFile "INSTALLSQLDATADIR=`"$env:choco:sqlserver2019:INSTALLSQLDATADIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SAPWD){Add-Content $configFile "SAPWD=`"$env:choco:sqlserver2019:SAPWD`""}
+    if (Test-Path env:\choco:sqlserver2019:SECURITYMODE){Add-Content $configFile "SECURITYMODE=`"$env:choco:sqlserver2019:SECURITYMODE`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLBACKUPDIR){Add-Content $configFile "SQLBACKUPDIR=`"$env:choco:sqlserver2019:SQLBACKUPDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLCOLLATION){Add-Content $configFile "SQLCOLLATION=`"$env:choco:sqlserver2019:SQLCOLLATION`""}
+    if (Test-Path env:\choco:sqlserver2019:ADDCURRENTUSERASSQLADMIN){Add-Content $configFile "ADDCURRENTUSERASSQLADMIN=`"$env:choco:sqlserver2019:ADDCURRENTUSERASSQLADMIN`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLSVCACCOUNT){Add-Content $configFile "SQLSVCACCOUNT=`"$env:choco:sqlserver2019:SQLSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLSVCPASSWORD){Add-Content $configFile "SQLSVCPASSWORD=`"$env:choco:sqlserver2019:SQLSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLSVCSTARTUPTYPE){Add-Content $configFile "SQLSVCSTARTUPTYPE=`"$env:choco:sqlserver2019:SQLSVCSTARTUPTYPE`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLSYSADMINACCOUNTS){Add-Content $configFile "SQLSYSADMINACCOUNTS=`"$env:choco:sqlserver2019:SQLSYSADMINACCOUNTS`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBDIR){Add-Content $configFile "SQLTEMPDBDIR=`"$env:choco:sqlserver2019:SQLTEMPDBDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBLOGDIR){Add-Content $configFile "SQLTEMPDBLOGDIR=`"$env:choco:sqlserver2019:SQLTEMPDBLOGDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBFILECOUNT){Add-Content $configFile "SQLTEMPDBFILECOUNT=`"$env:choco:sqlserver2019:SQLTEMPDBFILECOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBFILESIZE){Add-Content $configFile "SQLTEMPDBFILESIZE=`"$env:choco:sqlserver2019:SQLTEMPDBFILESIZE`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBFILEGROWTH){Add-Content $configFile "SQLTEMPDBFILEGROWTH=`"$env:choco:sqlserver2019:SQLTEMPDBFILEGROWTH`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBLOGFILESIZE){Add-Content $configFile "SQLTEMPDBLOGFILESIZE=`"$env:choco:sqlserver2019:SQLTEMPDBLOGFILESIZE`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLTEMPDBLOGFILEGROWTH){Add-Content $configFile "SQLTEMPDBLOGFILEGROWTH=`"$env:choco:sqlserver2019:SQLTEMPDBLOGFILEGROWTH`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLUSERDBDIR){Add-Content $configFile "SQLUSERDBDIR=`"$env:choco:sqlserver2019:SQLUSERDBDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLSVCINSTANTFILEINIT){Add-Content $configFile "SQLSVCINSTANTFILEINIT=`"$env:choco:sqlserver2019:SQLSVCINSTANTFILEINIT`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLUSERDBLOGDIR){Add-Content $configFile "SQLUSERDBLOGDIR=`"$env:choco:sqlserver2019:SQLUSERDBLOGDIR`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLMAXDOP){Add-Content $configFile "SQLMAXDOP=`"$env:choco:sqlserver2019:SQLMAXDOP`""}
+    if (Test-Path env:\choco:sqlserver2019:USESQLRECOMMENDEDMEMORYLIMITS){Add-Content $configFile "USESQLRECOMMENDEDMEMORYLIMITS=`"$env:choco:sqlserver2019:USESQLRECOMMENDEDMEMORYLIMITS`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLMINMEMORY){Add-Content $configFile "SQLMINMEMORY=`"$env:choco:sqlserver2019:SQLMINMEMORY`""}
+    if (Test-Path env:\choco:sqlserver2019:SQLMAXMEMORY){Add-Content $configFile "SQLMAXMEMORY=`"$env:choco:sqlserver2019:SQLMAXMEMORY`""}
+    if (Test-Path env:\choco:sqlserver2019:FILESTREAMLEVEL){Add-Content $configFile "FILESTREAMLEVEL=`"$env:choco:sqlserver2019:FILESTREAMLEVEL`""}
+    if (Test-Path env:\choco:sqlserver2019:FILESTREAMSHARENAME){Add-Content $configFile "FILESTREAMSHARENAME=`"$env:choco:sqlserver2019:FILESTREAMSHARENAME`""}
+    if (Test-Path env:\choco:sqlserver2019:FTSVCACCOUNT){Add-Content $configFile "FTSVCACCOUNT=`"$env:choco:sqlserver2019:FTSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:FTSVCPASSWORD){Add-Content $configFile "FTSVCPASSWORD=`"$env:choco:sqlserver2019:FTSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:ISSVCACCOUNT){Add-Content $configFile "ISSVCACCOUNT=`"$env:choco:sqlserver2019:ISSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:ISSVCPASSWORD){Add-Content $configFile "ISSVCPASSWORD=`"$env:choco:sqlserver2019:ISSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:ISSVCStartupType){Add-Content $configFile "ISSVCStartupType=`"$env:choco:sqlserver2019:ISSVCStartupType`""}
+    if (Test-Path env:\choco:sqlserver2019:NPENABLED){Add-Content $configFile "NPENABLED=`"$env:choco:sqlserver2019:NPENABLED`""}
+    if (Test-Path env:\choco:sqlserver2019:TCPENABLED){Add-Content $configFile "TCPENABLED=`"$env:choco:sqlserver2019:TCPENABLED`""}
+    if (Test-Path env:\choco:sqlserver2019:RSINSTALLMODE){Add-Content $configFile "RSINSTALLMODE=`"$env:choco:sqlserver2019:RSINSTALLMODE`""}
+    if (Test-Path env:\choco:sqlserver2019:RSSVCACCOUNT){Add-Content $configFile "RSSVCACCOUNT=`"$env:choco:sqlserver2019:RSSVCACCOUNT`""}
+    if (Test-Path env:\choco:sqlserver2019:RSSVCPASSWORD){Add-Content $configFile "RSSVCPASSWORD=`"$env:choco:sqlserver2019:RSSVCPASSWORD`""}
+    if (Test-Path env:\choco:sqlserver2019:RSSVCStartupType){Add-Content $configFile "RSSVCStartupType=`"$env:choco:sqlserver2019:RSSVCStartupType`""}
+    if (Test-Path env:\choco:sqlserver2019:MPYCACHEDIRECTORY){Add-Content $configFile "MPYCACHEDIRECTORY=`"$env:choco:sqlserver2019:MPYCACHEDIRECTORY`""}
+    if (Test-Path env:\choco:sqlserver2019:MRCACHEDIRECTORY){Add-Content $configFile "MRCACHEDIRECTORY=`"$env:choco:sqlserver2019:MRCACHEDIRECTORY`""}
+    if (Test-Path env:\choco:sqlserver2019:SQL_INST_JAVA){Add-Content $configFile "SQL_INST_JAVA=`"$env:choco:sqlserver2019:SQL_INST_JAVA`""}
+    
     
 }
 
