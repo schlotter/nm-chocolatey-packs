86ccf76a53f4f81d339fd4f0b7a1be783059856b
Bruno Braga <bruno@brunobraga.net>
2023-10-04 07:54:06 +1000

    Updated to version 10.2

--- fscapture/tools/chocolateyInstall.ps1
+++ fscapture/tools/chocolateyInstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= 'fscapture'
 $toolsDir   = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
-$url        = 'http://www.faststonesoft.net/DN/FSCaptureSetup90.exe'
+$url        = 'http://www.faststonesoft.net/DN/FSCaptureSetup102.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +12,7 @@ $packageArgs = @{
   silentArgs 	= "/S"
 
   softwareName  = 'FastStone Image Capture'
-  checksum      = 'AB3B0FDE4A162D81BD7D3520658B8F7DD95F7645'
+  checksum      = '7C8B8488BADE84660C421B42BF82E63AECD0DD1D'
   checksumType  = 'sha1'
 }
 
